26597c1676a16080d5ca402fe61424c0dff369b6
Eruslanov Andrei <eruslanov84as@gmail.com>
2026-09-01 14:18:48 +0300

    hw_2 Добавил блок try catch в блок Get

--- src/MainArray.java
+++ src/MainArray.java
@@ -4,6 +4,7 @@ import com.urise.webapp.storage.ArrayStorage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
 
 /**
  * Interactive test for com.urise.webapp.storage.ArrayStorage implementation
@@ -49,7 +50,11 @@ public class MainArray {
                     printAll();
                     break;
                 case "get":
-                    System.out.println(ARRAY_STORAGE.get(uuid));
+                    try {
+                        System.out.println(ARRAY_STORAGE.get(uuid));
+                    } catch (NoSuchElementException e) {
+                        System.out.println(e.getMessage());
+                    }
                     break;
                 case "clear":
                     ARRAY_STORAGE.clear();
