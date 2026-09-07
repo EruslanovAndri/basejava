162e1196f2402cb33a020fcf5bbf83869fdc7301
Eruslanov Andrei <eruslanov84as@gmail.com>
2026-09-07 13:10:51 +0300

    lesson_3 implemented the findResumeIndex method

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -1,5 +1,30 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
 public class SortedArrayStorage extends AbstractArrayStorage {
 
+    @Override
+    public void update(Resume resume) {
+
+    }
+
+    @Override
+    public void save(Resume resume) {
+
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    protected int findResumeIndex(String uuid) {
+        Resume searchResume = new Resume();
+        searchResume.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchResume);
+    }
 }
