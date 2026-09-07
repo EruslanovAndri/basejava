caae00e7a67d7c78139464ec7251ff1942caffa8
Eruslanov Andrei <eruslanov84as@gmail.com>
2026-09-07 11:54:16 +0300

    lesson_3 Реализовал интерфейс

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -9,7 +9,7 @@ import java.util.NoSuchElementException;
 /**
  * Array based storage for Resumes.
  */
-public class ArrayStorage {
+public class ArrayStorage implements Storage{
     private static final int STORAGE_LIMIT = 10000;
     private int size;
     public Resume[] storage = new Resume[STORAGE_LIMIT];
